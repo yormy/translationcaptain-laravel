52c3ab4aa9166d04285dce925a97a9db5fc2bdaf
rob <dev@mexion.com>
2020-11-22 19:52:32 +0100

    Removed old import from comment

--- src/Services/ImportLaravel.php
+++ src/Services/ImportLaravel.php
@@ -20,6 +20,9 @@ class ImportLaravel
 
     protected array $all;
 
+    const VENDOR_FILES = 0;
+    const APP_FILES = 1;
+
     public function __construct()
     {
         $this->app = App();
@@ -31,33 +34,16 @@ class ImportLaravel
         $importFromDir = $this->app['path.lang'];
 
         $languages =['en','nl','ar'];
-        $languages =['en'];
-
-       // $importFromDir .= "/vendor/";
-        //$importFromDir .= "/en/";
-        //dd($baseDir);
         foreach ($languages as $language) {
             $importFromLanguageDir = $importFromDir . DIRECTORY_SEPARATOR. $language;
-            $this->importAppFileTranslations($language, $importFromLanguageDir, $importFromLanguageDir);
+            $this->importFileTranslations(self::APP_FILES, $importFromLanguageDir, $importFromLanguageDir, $language);
         }
-        //$this->importVendorFileTranslations($importFromDir, $importFromDir);
+
+        $importFromVendorDir = $importFromDir . "/vendor";
+        $this->importFileTranslations(self::VENDOR_FILES, $importFromVendorDir, $importFromVendorDir, $language);
+
 dd($this->all);
         return $this->messages;
-    }
-
-    /**
-     * Recursive walk the directory tree and import the files according to
-     * the laravel package structure: lang/vendor/<vendorname>/<language>/group/group.php file stucture
-     *
-     * @param $root
-     * @param $baseDir
-     */
-    public function importVendorFileTranslations(string $root, string $importFromDir):  void
-    {
-        $this->addVendorFiles($root, $importFromDir);
-        foreach ($this->fileSystem->directories($importFromDir) as $currentDir) {
-            $this->importVendorFileTranslations($root, $currentDir);
-        }
     }
 
     /**
@@ -67,62 +53,19 @@ dd($this->all);
      * @param $root
      * @param $baseDir
      */
-    public function importAppFileTranslations(string $language, string $root, string $importFromDir) : void
+    public function importFileTranslations(int $directoryType, string $root, string $importFromDir, string $language) : void
     {
-        $this->addAppFiles($language, $root, $importFromDir);
+        if (!is_dir($importFromDir)) {
+            return;
+        }
+
+        $this->addTranslationFiles($directoryType, $root, $importFromDir, $language);
         foreach ($this->fileSystem->directories($importFromDir) as $currentDir) {
-            $this->importAppFileTranslations($language, $root, $currentDir);
+            $this->importFileTranslations($directoryType, $root, $currentDir, $language);
         }
     }
 
-    /**
-     * Loop through the entire import directory and add all keys in all files to the array
-     * @param string $root
-     * @param string $importFromDir
-     */
-    public function addVendorFiles(string $root, string $importFromDir)
-    {
-        foreach ($this->fileSystem->allfiles($importFromDir) as $file) {
-            $fullPathname = $file->getPathname();
-            $relativePathname = str_replace($root, '', $fullPathname);
-
-            // remove php extension
-            $relativePathname = substr($relativePathname,0, strlen($relativePathname) - strlen('.php'));
-
-            // strip leading directory separator
-            $first = substr($relativePathname, 0,1);
-            if ($first === DIRECTORY_SEPARATOR) {
-                $relativePathname = substr($relativePathname,1, strlen($relativePathname));
-            }
-
-            // determine filename vs keyname
-            $parts = explode(DIRECTORY_SEPARATOR, $relativePathname);
-
-            $package = $parts[0];
-            $language = $parts[1];
-            unset($parts[0]);
-            unset($parts[1]);
-
-            // All keys of this group are prefixed with entire directory path
-            $keyPrefix = implode('.', $parts);
-            if ($keyPrefix) {
-                $keyPrefix .= ".";
-            }
-
-            $keysForPackage = [];
-            $translations = $this->convertImportfileToArray($fullPathname);
-            foreach ($translations as $key => $translation)
-            {
-                $fullKey = $keyPrefix. $key;
-                $keysForPackage[$fullKey] = $translation;
-             }
-
-            $this->all[$language][$package] = $keysForPackage;
-        }
-    }
-
-
-    public function addAppFiles(string $language, string $root, string $langPath)
+    public function addTranslationFiles(int $directoryType, string $root, string $langPath, string $language)
     {
         foreach ($this->fileSystem->allfiles($langPath) as $file) {
 
@@ -142,13 +85,16 @@ dd($this->all);
             $parts = explode(DIRECTORY_SEPARATOR, $relative);
 
 
-
-            $group = $parts[0];
-            unset($parts[0]);
-
-
-
-
+            if (self::VENDOR_FILES=== $directoryType) {
+                $group = $parts[0];
+                $language = $parts[1];
+                unset($parts[0]);
+                unset($parts[1]);
+            }
+            if (self::APP_FILES=== $directoryType) {
+                $group = $parts[0];
+                unset($parts[0]);
+            }
 
             // All keys of this group are prefixed with entire directory path
             $keyPrefix = implode('.', $parts);
@@ -165,17 +111,6 @@ dd($this->all);
             }
 
             $this->all[$language][$group] = $keysForPackage;
-
-
-//            $translations = $this->convertImportfileToArray($fullPathname);
-//            if ($keyPrefix) {
-//                $keyPrefix .= ".";
-//            }
-//
-//            foreach ($translations as $key => $translation)
-//            {
-//                $this->messages[] = $group . " | ". $keyPrefix . "$key => $translation";
-//            }
         }
 
     }
@@ -202,126 +137,4 @@ dd($this->all);
 
     }
 
-//
-//    /**
-//     * @param bool $replace
-//     * @param null $base
-//     * @param bool $import_group
-//     * @return int|mixed
-//     */
-//    private function importAllFileTranslations($replace = false, $base = null, $import_group = false)
-//    {
-//        //https://github.com/barryvdh/laravel-translation-manager/blob/master/src/Manager.php
-//        $counter = 0;
-//        //allows for vendor lang files to be properly recorded through recursion.
-//        $vendor = true;
-//        if ($base == null) {
-//            $base = $this->app['path.lang'];
-//            $vendor = false;
-//        }
-//
-//        foreach ($this->files->directories($base) as $langPath) {
-//            $locale = basename($langPath);
-//
-//            //import langfiles for each vendor
-//            if ($locale == 'vendor') {
-//                foreach ($this->files->directories($langPath) as $vendor) {
-//                    $counter += $this->importAllFileTranslations($replace, $vendor);
-//                }
-//
-//                continue;
-//            }
-//            $vendorName = $this->files->name($this->files->dirname($langPath));
-//
-//            foreach ($this->files->allfiles($langPath) as $file) {
-//                $info = pathinfo($file);
-//                $group = $info['filename'];
-//                if ($import_group) {
-//                    if ($import_group !== $group) {
-//                        continue;
-//                    }
-//                }
-//
-////                if (in_array($group, $this->config['exclude_groups'])) {
-////                    continue;
-////                }
-//
-//                $subLangPath = str_replace($langPath. DIRECTORY_SEPARATOR, '', $info['dirname']);
-////                $subLangPath = str_replace(DIRECTORY_SEPARATOR, '/', $subLangPath);
-////                $langPath = str_replace(DIRECTORY_SEPARATOR, '/', $langPath);
-//
-//                if ($subLangPath != $langPath) {
-//                    $group = $subLangPath. DIRECTORY_SEPARATOR. $group;
-//                }
-//
-//                if (! $vendor) {
-//                    $translations = \Lang::getLoader()->load($locale, $group);
-//                } else {
-//                    $translations = include $file;
-//                    $group = 'vendor'.
-//                        DIRECTORY_SEPARATOR. $vendorName.
-//                        DIRECTORY_SEPARATOR. $locale.
-//                        DIRECTORY_SEPARATOR. $group;//.'/'.$file->getFilenameWithoutExtension();
-//                }
-//
-//                if ($translations && is_array($translations)) {
-//                    foreach (Arr::dot($translations) as $key => $value) {
-//                        $importedTranslation = $this->importTranslation($key, $value, $locale, $group, $replace);
-//                        $counter += $importedTranslation ? 1 : 0;
-//                    }
-//                }
-//            }
-//        }
-//
-//        foreach ($this->files->files($this->app['path.lang']) as $jsonTranslationFile) {
-//            if (strpos($jsonTranslationFile, '.json') === false) {
-//                continue;
-//            }
-//            $locale = basename($jsonTranslationFile, '.json');
-//            $group = self::JSON_GROUP;
-//            $translations =
-//                \Lang::getLoader()->load($locale, '*', '*'); // Retrieves JSON entries of the given locale only
-//            if ($translations && is_array($translations)) {
-//                foreach ($translations as $key => $value) {
-//                    $importedTranslation = $this->importTranslation($key, $value, $locale, $group, $replace);
-//                    $counter += $importedTranslation ? 1 : 0;
-//                }
-//            }
-//        }
-//
-//        return $counter;
-//    }
-//
-//    /**
-//     * @param $key
-//     * @param $value
-//     * @param $locale
-//     * @param $group
-//     * @param bool $replace
-//     * @return bool
-//     */
-//    private function importTranslation($dottedKey, $value, $locale, $group, $replace = false)
-//    {
-//        $ar = [$dottedKey => $value];
-//
-//        $treeKey = $this->arrayUndot($ar);
-//
-//        $new = array();
-//        $new[$locale][$group] = $treeKey;
-//
-//        $this->messages = array_merge_recursive($this->messages, $new);
-//
-//        return true;
-//    }
-//
-//
-//    public function arrayUndot($dotNotationArray)
-//    {
-//        $array = [];
-//        foreach ($dotNotationArray as $key => $value) {
-//            Arr::set($array, $key, $value);
-//        }
-//
-//        return $array;
-//    }
 }
