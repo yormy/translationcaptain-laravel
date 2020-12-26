815645439f5103963595eac98940f604d49c90ff
yormy <yormy@users.noreply.github.com>
2020-12-26 15:45:07 +0000

    Fix styling

--- src/Http/Controllers/PullController.php
+++ src/Http/Controllers/PullController.php
@@ -3,11 +3,7 @@
 namespace Yormy\TranslationcaptainLaravel\Http\Controllers;
 
 use Illuminate\Routing\Controller;
-use Illuminate\Support\Facades\Http;
-use Yormy\TranslationcaptainLaravel\Services\FileWriters\GeneratorBlade;
-use Yormy\TranslationcaptainLaravel\Services\FileWriters\GeneratorVue;
 use Yormy\TranslationcaptainLaravel\Services\PullService;
-use Yormy\TranslationcaptainLaravel\Services\PushService;
 
 class PullController extends Controller
 {
