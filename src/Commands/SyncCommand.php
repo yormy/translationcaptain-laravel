99d235f5f6449e0438ac3f7077f66755950a6565
yormy <yormy@users.noreply.github.com>
2020-12-27 11:02:24 +0000

    Fix styling

--- src/Commands/SyncCommand.php
+++ src/Commands/SyncCommand.php
@@ -3,11 +3,8 @@
 namespace Yormy\TranslationcaptainLaravel\Commands;
 
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\Artisan;
 use Yormy\TranslationcaptainLaravel\Commands\Traits\PullTrait;
 use Yormy\TranslationcaptainLaravel\Commands\Traits\PushTrait;
-use Yormy\TranslationcaptainLaravel\Services\PullService;
-use Yormy\TranslationcaptainLaravel\Services\PushService;
 
 class SyncCommand extends Command
 {
